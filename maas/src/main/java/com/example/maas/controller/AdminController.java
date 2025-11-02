ff9635226d4ee878fcbbf114e116f3111b926ecf
Mihai19 <mihai.popescu1109@gmail.com>
2025-11-02 15:03:11 +0200

    add admin actions

--- maas/src/main/java/com/example/maas/controller/AdminController.java
+++ maas/src/main/java/com/example/maas/controller/AdminController.java
@@ -1,4 +1,59 @@
 package com.example.maas.controller;
 
+import com.example.maas.entities.Role;
+import com.example.maas.repository.RoleRepository;
+import com.example.maas.repository.UserRepository;
+import com.example.maas.service.JwtService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/admin")
+@CrossOrigin(origins = "http://localhost:4200")
 public class AdminController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @DeleteMapping("/deleteUser/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public String deleteUser(@PathVariable("id") Long id) {
+        userRepository.deleteById(id);
+        return "User deleted successfully";
+    }
+
+    @PutMapping("/updateUserRole/{id}/{role}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public String updateUserRole(@PathVariable Long id, @PathVariable Role role) {
+        var user = userRepository.findById(id).orElseThrow(() -> new RuntimeException("User not found"));
+        user.setRole(roleRepository.getByName(role));
+        userRepository.save(user);
+        return "User role updated successfully";
+    }
+
+    @PutMapping("/resetPassword/{id}/{newPassword}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public String resetPassword(@PathVariable Long id, @PathVariable String newPassword) {
+        var user = userRepository.findById(id).orElseThrow(() -> new RuntimeException("User not found"));
+        user.setPassword(passwordEncoder.encode(newPassword));
+        userRepository.save(user);
+        return "Password reset successfully";
+    }
+
+    @PutMapping("/updateUsername/{id}/{newUsername}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public String updateUsername(@PathVariable Long id, @PathVariable String newUsername) {
+        var user = userRepository.findById(id).orElseThrow(() -> new RuntimeException("User not found"));
+        user.setUsername(newUsername);
+        userRepository.save(user);
+        return "Username updated successfully";
+    }
 }
--- maas/src/main/resources/db/migration/V1__init.sql
+++ maas/src/main/resources/db/migration/V1__init.sql
@@ -2,9 +2,9 @@ CREATE TABLE USERS (
     id SERIAL PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
     password VARCHAR(255) NOT NULL,
-    role VARCHAR(30) NOT NULL
+    role INTEGER NOT NULL
 );
 
 INSERT INTO USERS (username, password, role) VALUES
-('admin', '$2a$10$oBqEC5NavrePWeAIbAkEBezAx6TcmdNQaDUDstBNkQvBJwVYB6ae2', '1'),
-('user', '$2a$10$GLhC2wnSb2B9zezYTVy07eNVoLIaI8A9i5OzWAaRwmCzYSkfq7dtm', '3');
+('admin', '$2a$10$oBqEC5NavrePWeAIbAkEBezAx6TcmdNQaDUDstBNkQvBJwVYB6ae2', 1),
+('user', '$2a$10$GLhC2wnSb2B9zezYTVy07eNVoLIaI8A9i5OzWAaRwmCzYSkfq7dtm', 3);
