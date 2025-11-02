ac35680ef358e37baf3dc890ab17f1caa0601d70
Mihai19 <mihai.popescu1109@gmail.com>
2025-11-02 14:22:21 +0200

    add admin user and normal user in db

--- maas/src/main/resources/db/migration/V1__init.sql
+++ maas/src/main/resources/db/migration/V1__init.sql
@@ -6,5 +6,5 @@ CREATE TABLE USERS (
 );
 
 INSERT INTO USERS (username, password, role) VALUES
-('admin', 'adminpass', 'ADMIN'),
-('user', 'userpass', 'USER');
+('admin', '$2a$10$oBqEC5NavrePWeAIbAkEBezAx6TcmdNQaDUDstBNkQvBJwVYB6ae2', '1'),
+('user', '$2a$10$GLhC2wnSb2B9zezYTVy07eNVoLIaI8A9i5OzWAaRwmCzYSkfq7dtm', '3');
