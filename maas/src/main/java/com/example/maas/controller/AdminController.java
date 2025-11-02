60a9f709b6b64fdca877182199d083b0b5251a88
Mihai19 <mihai.popescu1109@gmail.com>
2025-11-02 14:14:33 +0200

    add admin user and normal user in db

---
+++ maas/src/main/java/com/example/maas/controller/AdminController.java
@@ -0,0 +1,4 @@
+package com.example.maas.controller;
+
+public class AdminController {
+}
--- maas/src/main/resources/db/migration/V1__init.sql
+++ maas/src/main/resources/db/migration/V1__init.sql
@@ -4,3 +4,7 @@ CREATE TABLE USERS (
     password VARCHAR(255) NOT NULL,
     role VARCHAR(30) NOT NULL
 );
+
+INSERT INTO USERS (username, password, role) VALUES
+('admin', 'adminpass', 'ADMIN'),
+('user', 'userpass', 'USER');
