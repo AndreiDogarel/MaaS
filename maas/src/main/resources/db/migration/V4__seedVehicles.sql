5f8223c84de3a5f4e51348c6d1121aa49cf4fe6f
gfa <florinalexandru302003@gmail.com>
2025-11-09 17:22:23 +0200

    add vehicle seeder migration

---
+++ maas/src/main/resources/db/migration/V4__seedVehicles.sql
@@ -0,0 +1,55 @@
+INSERT INTO VEHICLES (
+    REGISTRATION_NUMBER,
+    BRAND,
+    MODEL,
+    YEAR,
+    MILEAGE,
+    LICENSE_CATEGORY,
+    STATUS,
+    PRICE_PER_DAY
+) VALUES
+(
+    'XYZ-7890',
+    'Audi',
+    'A4 Sedan',
+    2023,
+    1500,
+    'B',
+    'AVAILABLE'
+),
+(
+    'TRE-1111',
+    'Tesla',
+    'Model 3',
+    2024,
+    500,
+    'B',
+    'AVAILABLE'
+),
+(
+    'QWE-2023',
+    'Mercedes-Benz',
+    'Sprinter Van',
+    2022,
+    35000,
+    'C',
+    'MAINTENANCE'
+),
+(
+    'FGH-5555',
+    'Honda',
+    'CRV SUV',
+    2021,
+    22000,
+    'B',
+    'AVAILABLE'
+),
+(
+    'JKL-9876',
+    'Volvo',
+    'FH Truck',
+    2019,
+    120000,
+    'D',
+    'UNAVAILABLE'
+);
