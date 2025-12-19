08729bec263497ab64d614924d4a056eb90b751b
gfa <florinalexandru302003@gmail.com>
2025-12-19 12:51:52 +0200

    made main-layout the single source of truth for FE

--- MaaS-front/src/app/app.component.ts
+++ MaaS-front/src/app/app.component.ts
@@ -1,15 +1,10 @@
 import { Component } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-import { HelloWorldComponent } from './hello-world/hello-world.component';
-import { UsersListComponent } from './users-list/users-list.component';
-import { VehicleListComponent } from "./vehicles/vehicle-list.component";
-import { VehicleDetailComponent } from './vehicles/vehicle-detail.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule, FormsModule, ReactiveFormsModule, HelloWorldComponent, UsersListComponent, VehicleListComponent, VehicleDetailComponent],
+  imports: [RouterModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
