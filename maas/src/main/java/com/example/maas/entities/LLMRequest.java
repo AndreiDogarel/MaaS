2102a00be635b727420ebff6fae69b3735e233ca
Mihai19 <mihai.popescu1109@gmail.com>
2026-01-02 17:34:21 +0200

    Add LLMController and related classes for LLM functionality

---
+++ maas/src/main/java/com/example/maas/entities/LLMRequest.java
@@ -0,0 +1,16 @@
+package com.example.maas.entities;
+
+public class LLMRequest {
+    private String prompt;
+
+    public LLMRequest() {
+    }
+
+    public String getPrompt() {
+        return prompt;
+    }
+
+    public void setPrompt(String prompt) {
+        this.prompt = prompt;
+    }
+}
